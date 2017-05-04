dff7ddbe98e9c2e38a683af6424dc2d3145392df
Daniel <danrica92@yahoo.com>
2017-05-04 14:28:33 -0700

    Testing Code Update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Viral Labs')
+  res.send('This is a Test')
 })
 
 app.listen(3000, function () {
