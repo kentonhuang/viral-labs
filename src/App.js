061bc720d60d5dcc5f9abf20b0cbac62a33be733
Daniel <danrica92@yahoo.com>
2017-05-04 17:17:16 -0700

    Changed Start in package.json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Viral Labs Test</h1>
+        <h1>Viral Labs</h1>
         <a href="http://viral-laboratories.com">Home</a>
       </div>
     );
