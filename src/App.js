e4c75873c2188089cccc6df59ba6bef25c257044
Daniel <danrica92@yahoo.com>
2017-05-05 00:28:55 -0700

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Viral Labs</h1>
-        <a href="http://viral-laboratories.com">Home</a>
+        <p className="title">VIRAL LABORATORIES</p>
       </div>
     );
   }
